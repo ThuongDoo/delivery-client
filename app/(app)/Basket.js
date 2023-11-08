ca8371fa13f576779e968f9189cc790333888e9b
Út <ut@Heaven-2.local>
2023-11-08 17:24:23 +0700

    update basket

--- app/(app)/Basket.js
+++ app/(app)/Basket.js
@@ -1,4 +1,11 @@
-import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Button,
+} from "react-native";
 import React, { useEffect, useId, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectBasketItem, selectBasketTotal } from "../../slices/basketSlice";
@@ -10,6 +17,8 @@ import CurrencyFormatter from "../../components/CurrencyFormatter";
 import { Entypo } from "@expo/vector-icons";
 import { iconColor } from "../../utils/constants";
 import { useFocusEffect } from "expo-router";
+import { Field, Form, Formik } from "formik";
+import { CheckBox } from "@rneui/themed";
 
 const Basket = () => {
   const [basket, setBasket] = useState([]);
@@ -23,7 +32,7 @@ const Basket = () => {
         api
           .get(`/basket/${user.userId}`)
           .then((res) => {
-            console.log(res.data.items);
+            // console.log(res.data.items);
             setBasket(res.data.items);
             const tempRestaurant = res.data.items.map(
               (item) => item.food.restaurant
@@ -42,7 +51,7 @@ const Basket = () => {
     }, [useId])
   );
   // console.log(basket);
-  console.log(restaurant);
+  // console.log(restaurant);
 
   const onChange = (value) => {
     const updateBasket = basket.map((item) => {
@@ -51,7 +60,7 @@ const Basket = () => {
       }
       return item;
     });
-    console.log(updateBasket);
+    // console.log(updateBasket);
     setBasket(updateBasket);
   };
 
@@ -81,49 +90,77 @@ const Basket = () => {
   }, [basket]);
 
   return (
-    <View className="h-full">
-      {basketQuantity !== 0 && (
-        <TouchableOpacity className="px-3 bg-customRed absolute bottom-2 left-0 right-0 z-50 flex-row rounded-lg space-x-1 items-center mx-5 py-4">
-          <Text className="text-lg font-extrabold text-white basis-1/4">
-            Order
-          </Text>
-          <Text className="text-lg text-white font-extrabold text-center flex-1">
-            {CurrencyFormatter({ amount: basketTotalPrice })}
-          </Text>
-          <Text className="text-lg text-white font-extrabold text-right basis-1/4">
-            {basketQuantity}
-          </Text>
-        </TouchableOpacity>
+    <Formik
+      initialValues={{ isChecked: [] }}
+      onSubmit={(values) => {
+        // console.log(values.isChecked);
+        const checkedFoods = Object.keys(values.isChecked).filter(
+          (key) => values.isChecked[key] === true
+        );
+        console.log(checkedFoods);
+      }}
+    >
+      {({ values, handleChange, setFieldValue, handleSubmit }) => (
+        <View className="h-full">
+          {basketQuantity !== 0 && (
+            <TouchableOpacity
+              className="px-3 bg-customRed absolute bottom-2 left-0 right-0 z-50 flex-row rounded-lg space-x-1 items-center mx-5 py-4"
+              onPress={handleSubmit}
+            >
+              <Text className="text-lg font-extrabold text-white basis-1/4">
+                Order
+              </Text>
+              <Text className="text-lg text-white font-extrabold text-center flex-1">
+                {CurrencyFormatter({ amount: basketTotalPrice })}
+              </Text>
+              <Text className="text-lg text-white font-extrabold text-right basis-1/4">
+                {basketQuantity}
+              </Text>
+            </TouchableOpacity>
+          )}
+          <ScrollView
+            className="px-3"
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 80 }}
+          >
+            {restaurant?.map((restaurantItem) => (
+              <View key={restaurantItem._id}>
+                <View className="flex-row items-center space-x-2 py-2 border-b">
+                  <Image
+                    source={{ uri: restaurantItem.image }}
+                    className="w-7 h-7"
+                  />
+                  <Text>{restaurantItem.name}</Text>
+                </View>
+
+                {basket
+                  .filter(
+                    (item) => item.food.restaurant._id === restaurantItem._id
+                  )
+                  .map((filteredItem) => (
+                    <View key={filteredItem._id}>
+                      <BasketRow
+                        data={filteredItem}
+                        userId={user.userId}
+                        total={onChange}
+                        itemDelete={handleDelete}
+                        isChecked={values.isChecked[filteredItem._id] || false}
+                        onCheckChange={(itemId) => {
+                          console.log(itemId);
+                          setFieldValue(
+                            `isChecked.${itemId}`,
+                            !values.isChecked[itemId]
+                          );
+                        }}
+                      />
+                    </View>
+                  ))}
+              </View>
+            ))}
+          </ScrollView>
+        </View>
       )}
-      <ScrollView
-        className="px-3"
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 80 }}
-      >
-        {restaurant?.map((restaurantItem) => (
-          <View key={restaurantItem._id}>
-            <View className="flex-row items-center space-x-2 py-2 border-b">
-              <Image
-                source={{ uri: restaurantItem.image }}
-                className="w-7 h-7"
-              />
-              <Text>{restaurantItem.name}</Text>
-            </View>
-            {basket
-              .filter((item) => item.food.restaurant._id === restaurantItem._id)
-              .map((filteredItem) => (
-                <BasketRow
-                  key={filteredItem._id}
-                  data={filteredItem}
-                  userId={user.userId}
-                  total={onChange}
-                  itemDelete={handleDelete}
-                />
-              ))}
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+    </Formik>
   );
 };
 
--- components/BasketRow.js
+++ components/BasketRow.js
@@ -5,8 +5,17 @@ import { iconColor } from "../utils/constants";
 import CurrencyFormatter from "./CurrencyFormatter";
 import api from "../utils/api";
 import { Feather } from "@expo/vector-icons";
+import { Formik } from "formik";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const BasketRow = ({ data, userId, total, itemDelete }) => {
+const BasketRow = ({
+  data,
+  userId,
+  total,
+  itemDelete,
+  isChecked,
+  onCheckChange,
+}) => {
   const [quantity, setQuantity] = useState(data.quantity);
   const [loading, setLoading] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
@@ -50,13 +59,15 @@ const BasketRow = ({ data, userId, total, itemDelete }) => {
     setQuantity(data.quantity);
   }, [data.quantity]);
 
+  // console.log(data);
+
   if (isDelete) return;
   return (
     <View className="flex-row items-center border-b border-t border-gray-200 h-32 gap-x-3">
       <TouchableOpacity className="" onPress={deleteItem}>
-        <Feather name="x" size={24} color="red" />
+        <Feather name="x" size={24} color={iconColor} />
       </TouchableOpacity>
-      <View className=" flex-1">
+      <View className="flex-1">
         <Text className="text-lg font-bold">{data.food.name}</Text>
         <Text className="font-bold text-red-500 text-lg">
           {CurrencyFormatter({ amount: data.food.price })}
@@ -81,6 +92,13 @@ const BasketRow = ({ data, userId, total, itemDelete }) => {
           className="w-20 h-20 rounded-xl"
         />
       </View>
+      <TouchableOpacity onPress={() => onCheckChange(data._id)}>
+        <MaterialCommunityIcons
+          name={isChecked ? "checkbox-intermediate" : "checkbox-blank-outline"}
+          size={24}
+          color={iconColor}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
